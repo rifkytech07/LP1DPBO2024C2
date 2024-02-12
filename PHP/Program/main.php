036525fe488b49a777def9137c640063f99de5d5
faisal risa <muhammadrifkyafandi@upi.edu>
2024-02-12 09:50:53 +0700

    update LP1DPBO 2024C2 PHP

--- PHP/Program/main.php
+++ PHP/Program/main.php
@@ -7,16 +7,17 @@
 <?php
 require('Anggota.php'); //import file
 
-//hubungkan dengan file style.css.
-echo '<link rel="stylesheet" href="style.css">';
 
-//array
-$daftar = array();;
+
 
 //Menambahkan data
  $dpr1 = new Anggota('111', 'RIfky', 'IT', 'PKS', 'Foto Profil/PP1.jpg');
  $dpr2 = new Anggota('112', 'Caca', 'Pendidikan', 'PKB', 'Foto Profil/PP2.jpg');
  $dpr3 = new Anggota('113', 'Layla', 'Ekonomi', 'Nasdem', 'Foto Profil/PP3.png');
+
+ //array
+$daftar = array();
+
  
 //setelah itu, masukkan ke dalam list
  array_push($daftar, $dpr1);
@@ -24,26 +25,26 @@ $daftar = array();;
  array_push($daftar, $dpr3);
 
 //menampilkan list
- echo "<h1 style='text-align: center;'>Daftar Anggota DPR Republik Indonesia</h1>";
- echo "<table id = 'tabel'>";
-    echo "<tr>";
-    echo "<th>ID</th>";
-    echo "<th>Nama</th>";
-    echo "<th>Nama Bidang</th>";
-    echo "<th>Nama Partai</th>";
-    echo "<th>Foto</th>";
-    echo "</tr>";
+echo "<h1>Daftar Anggota DPR Republik Indonesia</h1>";
+echo "<table border = 'black'>";
+   echo "<tr>";
+   echo "<th>ID</th>";
+   echo "<th>Nama</th>";
+   echo "<th>Nama Bidang</th>";
+   echo "<th>Nama Partai</th>";
+   echo "<th>Foto</th>";
+   echo "</tr>";
 
 
-    foreach($daftar as $wakil){
-        echo "<tr>";
-        echo "<td>" . $wakil->getId() . "</td>";
-        echo "<td>" . $wakil->getNama() . "</td>";
-        echo "<td>" . $wakil->getBidang() . "</td>";
-        echo "<td>" . $wakil->getPartai() . "</td>";
-        echo "<td><img src='" . $wakil->getFoto() . "' width='60px' height='100px'></td>";
-        echo "</tr>";
-    }
+   foreach($daftar as $wakil){
+       echo "<tr>";
+       echo "<td>" . $wakil->getId() . "</td>";
+       echo "<td>" . $wakil->getNama() . "</td>";
+       echo "<td>" . $wakil->getBidang() . "</td>";
+       echo "<td>" . $wakil->getPartai() . "</td>";
+       echo "<td><img src='" . $wakil->getFoto() . "' width='60px' height='100px'></td>";
+       echo "</tr>";
+   }
 
     echo "</table>";
 
@@ -51,18 +52,16 @@ $daftar = array();;
     $tukar_id = "112";
 
     foreach($daftar as $wakil){
-        if($wakil->getId() == $tukar_id){
-            $wakil->setnama('Celine');
-            $wakil->setBidang('Pertahanan');
-            $wakil->setPartai('Gerindra');
-            $wakil->setFoto('Foto Profil/PP4.jpg');
-        }
+        $wakil->setnama('Celine');
+        $wakil->setBidang('Pertahanan');
+        $wakil->setPartai('Gerindra');
+        $wakil->setFoto('Foto Profil/PP4.jpg');
     }
 
     //setelah proses pengubahan, tampilkan kembali list nya
-    echo "<h4 style='text-align: center;'>Data Berhasil Diubah</h4>";
-    echo "<h1 style='text-align: center;'>Daftar Anggota DPR Republik Indonesia</h1>";
-    echo "<table id = 'tabel'>";
+    echo "<h4>Data Berhasil Diubah</h4>";
+    echo "<h1>Daftar Anggota DPR Republik Indonesia</h1>";
+    echo "<table border = 'black'>";
         echo "<tr>";
         echo "<th>ID</th>";
         echo "<th>Nama</th>";
@@ -93,9 +92,9 @@ $daftar = array();;
     }
 
     //setelahproses penghapusan data, tampilkan kembali listnya
-    echo "<h4 style='text-align: center;'>Data Berhasil Dihapus</h4>";
-    echo "<h1 style='text-align: center;'>Daftar Anggota DPR Republik Indonesia</h1>";
-    echo "<table id = 'tabel'>";
+    echo "<h4>Data Berhasil Dihapus</h4>";
+    echo "<h1>Daftar Anggota DPR Republik Indonesia</h1>";
+    echo "<table border = 'black'>";
         echo "<tr>";
         echo "<th>ID</th>";
         echo "<th>Nama</th>";
